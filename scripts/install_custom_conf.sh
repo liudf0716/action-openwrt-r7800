7dc3e47508ff7875ffaeef85d2f49652cca6173e
Dengfeng Liu <liudf0716@gmail.com>
2022-01-10 14:23:30 +0800

    Update install_custom_conf.sh

--- scripts/install_custom_conf.sh
+++ scripts/install_custom_conf.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
 install_custom_conf() {
-  cp ../apfree-wifidog/config/wifidogx package/feeds/packages/apfree-wifidog/files/wifidogx.conf
-  cp ../frpc/config/frpc package/feeds/packages/frp/files/frpc.config
-  cp ../kcptun/config/kcptun package/feeeds/packages/kcptun/files/kcptun.config
-  cp ../ss-libev/config/shadowsocks-libev package/feeeds/packages/shadowsocks-libev/files/shadowsocks-libev.config
-  cp ../uhttpd/config/uhttpd  package/network/services/uhttpd/files/uhttpd.config
+  if [ -e "package/feeds/packages/apfree-wifidog/files/wifidogx.conf"]
+  then
+    cp ../apfree-wifidog/config/wifidogx package/feeds/packages/apfree-wifidog/files/wifidogx.conf
+  else
+    echo "apfree-wifidog not selected"
+  fi
+  
+  if [ -e "package/feeds/packages/frp/files/frpc.config"]  
+  then
+    cp ../frpc/config/frpc package/feeds/packages/frp/files/frpc.config
+  else
+    echo "frp not selected"
+  fi
+  
+  if [ -e "package/feeeds/packages/kcptun/files/kcptun.config"]  
+  then
+    cp ../kcptun/config/kcptun package/feeeds/packages/kcptun/files/kcptun.config
+  else
+    echo "kcptun not selected"
+    ls package/feeeds/packages/kcptun/files/
+  fi
+  
+  if [ -e "package/feeeds/packages/shadowsocks-libev/files/shadowsocks-libev.config"]  
+  then
+    cp ../ss-libev/config/shadowsocks-libev package/feeeds/packages/shadowsocks-libev/files/shadowsocks-libev.config
+  else
+    echo "shadowsocks not selected"
+    ls package/feeeds/packages/shadowsocks-libev/files/
+  fi
+  
+  if [ -e "package/network/services/uhttpd/files/uhttpd.config"]  
+  then
+    cp ../uhttpd/config/uhttpd  package/network/services/uhttpd/files/uhttpd.config
+  else
+    echo "uhttpd not selected"
+  fi 
 }
 
 install_custom_conf
